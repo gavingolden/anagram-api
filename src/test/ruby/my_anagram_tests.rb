a9f621472212b4eb060a275230104ffe66252291
gavingolden <gavinegolden@gmail.com>
2018-07-18 19:51:20 -0400

    [158895434] Add tests for anagram comparison endpoint

---
+++ src/test/ruby/my_anagram_tests.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require_relative 'given_anagram_client'
+require 'test/unit'
+
+# capture ARGV before TestUnit Autorunner clobbers it
+
+class TestCases < Test::Unit::TestCase
+
+  # runs before each test
+  def setup
+    @client = GivenAnagramClient.new(ARGV)
+
+    # add words to the dictionary
+    @client.post('/words.json', nil, {"words" => ["read", "dear", "dare"]}) rescue nil
+  end
+
+  # runs after each test
+  def teardown
+    # delete everything
+    @client.delete('/words.json') rescue nil
+  end
+
+  def test_comparing_words__valid_words__match
+
+    res = @client.post('/anagrams/comparison.json', nil, {"words" => ["read", "dear", "dare"]}) rescue nil
+
+    assert_equal('200', res.code, "Unexpected response code")
+
+    body = JSON.parse(res.body)
+
+    assert_equal(true, body['match'])
+  end
+
+  def test_comparing_words__unknown_word__no_match
+
+    unknown_word = 'dare'
+
+    @client.delete("/words/#{unknown_word}.json") rescue nil
+
+    res = @client.post('/anagrams/comparison.json', nil, {"words" => [ unknown_word ]}) rescue nil
+
+    assert_equal('200', res.code, "Unexpected response code")
+
+    body = JSON.parse(res.body)
+
+    assert_equal(false, body['match'])
+  end
+
+end
