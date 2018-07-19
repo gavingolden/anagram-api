c75bc4abc16a463aa1b7c3988de5ff48319abb02
gavingolden <gavinegolden@gmail.com>
2018-07-19 15:48:01 -0400

    Add documentation to controller endpoints

--- src/main/kotlin/anagram/api/resource/words/WordsController.kt
+++ src/main/kotlin/anagram/api/resource/words/WordsController.kt
@@ -11,18 +11,27 @@ class WordsController(val wordsService: Corpus) {
 
     data class WordsRequest(val words: Collection<String>)
 
+    /**
+     * Add words to the corpus
+     */
     @PostMapping("/words.json")
     @ResponseStatus(HttpStatus.CREATED)
     fun addWord(@RequestBody body: WordsRequest) {
         wordsService.addWords(body.words)
     }
 
+    /**
+     * Remove words from the corpus
+     */
     @DeleteMapping("/words.json")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteWords() {
         wordsService.clearCorpus()
     }
 
+    /**
+     * Delete a word from the corpus
+     */
     @DeleteMapping("/words/{word}.json")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteWord(@PathVariable("word") word: String) {
@@ -32,6 +41,9 @@ class WordsController(val wordsService: Corpus) {
 
     data class AnagramsResponse(val anagrams: Collection<String>)
 
+    /**
+     * Get the anagrams of the given word
+     */
     @GetMapping("/anagrams/{word}.json")
     @ResponseStatus(HttpStatus.OK)
     fun anagrams(@PathVariable("word") word: String,
@@ -41,6 +53,9 @@ class WordsController(val wordsService: Corpus) {
         return AnagramsResponse(wordsService.findAnagrams(word, limit, excludeProperNouns))
     }
 
+    /**
+     * Delete the word and its anagrams
+     */
     @DeleteMapping("/anagrams/{word}.json")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun anagrams(@PathVariable("word") word: String) {
@@ -49,6 +64,9 @@ class WordsController(val wordsService: Corpus) {
 
     data class ComparisonResponse(val match: Boolean)
 
+    /**
+     * Determine if the given words are anagrams of one another
+     */
     @PostMapping("/anagrams/comparison.json")
     @ResponseStatus(HttpStatus.OK)
     fun comparison(@RequestBody body: WordsRequest): ComparisonResponse {
